8dd20e90fda01f74e8d62f8b47bfb6e7e802a7a2
Black Guy <samiiiiiiiiiiiiiiiiiiiiiiii24@gmail.com>
2016-06-21 23:36:34 -0700

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -254,7 +254,7 @@ function create_config( )
       "portuguese_lang",
       "arabic"
      },
-    sudo_users = {136888679},
+    sudo_users = {203007139},
     admin_users = {},
     disabled_channels = {}
   }
